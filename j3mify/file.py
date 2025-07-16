8d765ddff325f2f82a63e5987f0da15476278542
Haimonmon <beankor8@gmail.com>
2025-07-16 16:44:05 +0800

    Added txt file loader

--- j3mify/file.py
+++ j3mify/file.py
@@ -10,6 +10,12 @@ def load_file(file_name: str) -> List | Dict | Any:
         return json.load(file)
 
 
+def load_txt_file(file_name: str) -> List[str]:
+    """ Loads data on the specified txt file name. """
+    with open(fr"{folder_directory}{file_name}", "r", encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip()]
+
+
 def save_file(file_name: str, data: Any) -> Any:
     """ Saves data on the specified json file name. """
     with open(file_name, 'w') as file:
