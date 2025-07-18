d0a3ed6cdc25d4c624ad9f880051369a4dd42fc5
Haimonmon <beankor8@gmail.com>
2025-07-18 16:25:04 +0800

    Now supports emoticons

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import j3mify as jeje
 
 def main():
     """ main terminal 💀👌🥐✨ """
-    sentence1: str = "H1ndI p0 4kO nA9s45al1tA nA9t4typ3 p0 4kooo"
+    sentence1: str = "H1ndI p0 4kO nA9s45al1tA:p nA9t4typ3 p0 4kooo ;3"
 
     sentence2: str = "muuzt4hH gudnyt" # ! Still need a fixing on these to fix this sentence
 
     sentence3: str = "m4ta9al na 4kon9 j3j3mon 3h jajajajajajajaja"
+    
+    sentence4: str = ":3"
 
-    jeje.jejenized(jeje_sentence = sentence3)
+    print(jeje.jejenized(sentence = sentence1))
 
 if __name__ == "__main__":
     main()
