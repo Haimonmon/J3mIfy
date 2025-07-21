0e32d3071a6fdde9fb0fd36d83dd142f859b2ac4
Haimonmon <beankor8@gmail.com>
2025-07-21 23:07:52 +0800

    Modified: Testing

--- j3mify/main.py
+++ j3mify/main.py
@@ -1,14 +1,18 @@
 from .correction import best_match
-from .file import load_file, save_file
-from .normalization import normalization, tokenization, normal_words, List
+from .normalization import normalization, tokenization, normal_words
 
-from typing import Literal
+from typing import Literal, List
 
 def jejenized(sentence: str, mode: Literal["normal", "presentation", "debug"] = None) -> None:
     """ Converts jejemon sentence into normal sentence """
+    # * NORMALIZING
     normalized_characters: str = normalization(sentence = sentence)
+    print(normalized_characters)
+    # * TOKENIZATION
     tokenized: List[str] = tokenization(normalized_characters)
+    print(tokenized)
 
+    # * FUZZY MATCH
     correct_match: List[str] = [best_match(word = word, choices = normal_words) for word in tokenized]
     
     normalized_sentence: str = " ".join(correct_match)
