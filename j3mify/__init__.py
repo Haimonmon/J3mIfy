a0efb38992172b853883ec45077e32c3e3ae728b
Haimonmon <beankor8@gmail.com>
2025-08-01 19:38:53 +0800

    Modified: change function export names

--- j3mify/__init__.py
+++ j3mify/__init__.py
@@ -3,9 +3,9 @@ Jejemon Normalizer
 """
 
 from .main import jejenized
-from .correction import best_match
+from .correction import best_normal_match, tokenization
 from .file import load_file, save_file
-from .normalization import normalization, tokenization
+from .normalization import normalization
 
 __all__ = [
       "jejenized"
